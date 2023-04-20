fd7746407a724a75658382849bb1be9bb1a9f725
Geanta Adrian <geanta300@gmail.com>
2023-04-20 15:24:40 +0300

    Commit

--- patient-board-app/src/main/java/patients_board/SpringApp.java
+++ patient-board-app/src/main/java/patients_board/SpringApp.java
@@ -1,18 +1,26 @@
 package patients_board;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import model.Patient;
+import repo.PatientRepo;
 import java.util.List;
 
 @SpringBootApplication
 public class SpringApp implements CommandLineRunner {
+    @Autowired
+    PatientRepo patientRepo;
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        List<Patient> patients = patientRepo.findPatientsByHospitalId((long) 2);
+        for (Patient patient : patients) {
+            System.out.println(patient.getName());
+        }
     }
 }
