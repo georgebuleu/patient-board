dd3292752e3f0505460f22828e575cce4599186b
george <georgebu24@gmail.com>
2023-04-20 16:26:55 +0300

    fixed pom and postgresql issues

--- patient-board-app/src/main/java/patients_board/SpringApp.java
+++ patient-board-app/src/main/java/patients_board/SpringApp.java
@@ -1,13 +1,12 @@
 package patients_board;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "model")
 public class SpringApp {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
     }
