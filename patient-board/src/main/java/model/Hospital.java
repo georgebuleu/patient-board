59cd3d99330ac12c82e1ce4bf4dcac5d32b80fea
george <georgebu24@gmail.com>
2023-04-20 13:09:25 +0300

    added Hospital and Room entities

--- patient-board/src/main/java/model/Hospital.java
+++ patient-board/src/main/java/model/Hospital.java
@@ -7,28 +7,17 @@ import jakarta.persistence.*;
 public class Hospital {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long hospitalID;
-    @Column(name = "name")
+    private Long id;
     private String name;
-    @Column(name = "address")
     private String address;
-    @Column(name = "email")
     private String email;
-    @Column(name = "phone")
     private String phone;
-
-    public Hospital() {
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public Hospital(String name, String address, String email, String phone) {
-        this.name = name;
-        this.address = address;
-        this.email = email;
-        this.phone = phone;
-    }
-
-    public Long getHospitalID() {
-        return hospitalID;
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
