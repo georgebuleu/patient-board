4974ccd7c3145af6d48e88e6b61f2a499f1a4cbe
george <georgebu24@gmail.com>
2023-04-20 16:41:38 +0300

    deleted unnecessary imports

--- patient-board-app/src/main/java/patients_board/SpringApp.java
+++ patient-board-app/src/main/java/patients_board/SpringApp.java
@@ -2,7 +2,6 @@ package patients_board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "model")
