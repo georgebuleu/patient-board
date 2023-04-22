0779c9ac3a66ec1f74e35ff00ec4158c7e0711a7
_mtelea <telea.maria3@gmail.com>
2023-04-22 22:57:30 +0300

    create medical history model

--- patient-board/src/main/java/model/MedicalHistory.java
+++ patient-board/src/main/java/model/MedicalHistory.java
@@ -1,5 +1,6 @@
 package model;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
 
 @Entity
@@ -9,4 +10,108 @@ public class MedicalHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long medicalHistoryID;
+
+    @Column
+    private String heredoColHistory;
+
+    @Column
+    @Nonnull
+    private String livingWorkingConditions;
+
+    @Column
+    @Nonnull
+    private String behaviours ;
+
+    @Column
+    private String backgroundMeds;
+
+    @Column
+    private String diseaseHistory;
+
+    @Column
+    @Nonnull
+    private String generalExam;
+
+    public MedicalHistory() { }
+
+    public MedicalHistory(Long medicalHistoryID, String heredoColHistory, String livingWorkingConditions, String behaviours,
+                          String backgroundMeds, String diseaseHistory, String generalExam) {
+
+        this.medicalHistoryID = medicalHistoryID;
+        this.heredoColHistory = heredoColHistory;
+        this.livingWorkingConditions = livingWorkingConditions;
+        this.behaviours = behaviours;
+        this.backgroundMeds = backgroundMeds;
+        this.diseaseHistory = diseaseHistory;
+        this.generalExam = generalExam;
+    }
+
+    public Long getMedicalHistoryID() {
+        return medicalHistoryID;
+    }
+
+    public void setMedicalHistoryID(Long medicalHistoryID) {
+        this.medicalHistoryID = medicalHistoryID;
+    }
+
+    public String getHeredoColHistory() {
+        return heredoColHistory;
+    }
+
+    public void setHeredoColHistory(String heredoColHistory) {
+        this.heredoColHistory = heredoColHistory;
+    }
+
+    public String getLivingWorkingConditions() {
+        return livingWorkingConditions;
+    }
+
+    public void setLivingWorkingConditions(String livingWorkingConditions) {
+        this.livingWorkingConditions = livingWorkingConditions;
+    }
+
+    public String getBehaviours() {
+        return behaviours;
+    }
+
+    public void setBehaviours(String behaviours) {
+        this.behaviours = behaviours;
+    }
+
+    public String getBackgroundMeds() {
+        return backgroundMeds;
+    }
+
+    public void setBackgroundMeds(String backgroundMeds) {
+        this.backgroundMeds = backgroundMeds;
+    }
+
+    public String getDiseaseHistory() {
+        return diseaseHistory;
+    }
+
+    public void setDiseaseHistory(String diseaseHistory) {
+        this.diseaseHistory = diseaseHistory;
+    }
+
+    public String getGeneralExam() {
+        return generalExam;
+    }
+
+    public void setGeneralExam(String generalExam) {
+        this.generalExam = generalExam;
+    }
+
+    @Override
+    public String toString() {
+        return "MedicalHistory{" +
+                "medicalHistoryID=" + medicalHistoryID +
+                ", heredoColHistory='" + heredoColHistory + '\'' +
+                ", livingWorkingConditions='" + livingWorkingConditions + '\'' +
+                ", behaviours='" + behaviours + '\'' +
+                ", backgroundMeds='" + backgroundMeds + '\'' +
+                ", diseaseHistory='" + diseaseHistory + '\'' +
+                ", generalExam='" + generalExam + '\'' +
+                '}';
+    }
 }
