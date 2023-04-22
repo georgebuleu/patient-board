23f244df5b2ee935c38cb0ae75a15b31bf5519fa
george <george-alexandru.buleu@student.tuiasi.ro>
2023-04-22 23:56:19 +0300

    added the necessary relations

--- patient-board/src/main/java/model/Treatment.java
+++ patient-board/src/main/java/model/Treatment.java
@@ -8,5 +8,7 @@ public class Treatment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long treatmentId;
-
+    @ManyToOne
+    @JoinColumn(name = "diagnosis_id")
+    private Diagnosis diagnosis;
 }
