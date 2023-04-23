f4e559025a60c36dbd9896bd398780c685265191
biancamrd <e.bibu.e.b@gmail.com>
2023-04-23 18:00:36 +0300

    update hospitalStaff entity model

--- patient-board/src/main/java/model/HospitalStaff.java
+++ patient-board/src/main/java/model/HospitalStaff.java
@@ -1,6 +1,7 @@
 package model;
 
 import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
 
 @Entity
 @Table(name = "hospitalStaff")
@@ -8,4 +9,77 @@ public class HospitalStaff {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long hospitalStaffId;
+
+    @NonNull
+    @Column(name = "name")
+    private String name;
+
+    @NonNull
+    @Column(name = "email")
+    private String email;
+
+    @NonNull
+    @Column(name = "password")
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "specialityID")
+    private Speciality speciality;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "functionId")
+    private Function function;
+
+    public HospitalStaff() {
+    }
+
+
+
+    public Long getHospitalStaffId() {
+        return hospitalStaffId;
+    }
+
+    public void setHospitalStaffId(Long hospitalStaffId) {
+        this.hospitalStaffId = hospitalStaffId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Speciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
+
+    public Function getFunction() {
+        return function;
+    }
+
+    public void setFunction(Function function) {
+        this.function = function;
+    }
 }
