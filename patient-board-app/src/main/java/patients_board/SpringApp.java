03651e3e24bbb5f80a38a24a720896676a6919e0
george <george-alexandru.buleu@student.tuiasi.ro>
2023-04-26 09:00:27 +0300

    updated package name

--- patient-board-app/src/main/java/patients_board/SpringApp.java
+++ patient-board-app/src/main/java/patients_board/SpringApp.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("application")
-@ComponentScan("application")
-@EnableJpaRepositories("application")
+@EntityScan("com.cegeka.academy.patienthub")
+@ComponentScan("com.cegeka.academy.patienthub")
+@EnableJpaRepositories("com.cegeka.academy.patienthub")
 public class SpringApp {
 
     public static void main(String[] args) {
