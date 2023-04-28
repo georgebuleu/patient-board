ce7fcd04cbbe97ab3a63ca35137d9e525983dd30
george <george-alexandru.buleu@student.tuiasi.ro>
2023-04-29 00:05:15 +0300

    refactored to record

--- patient-board/src/main/java/com/cegeka/academy/patienthub/dto/User.java
+++ patient-board/src/main/java/com/cegeka/academy/patienthub/dto/User.java
@@ -1,30 +1,5 @@
 package com.cegeka.academy.patienthub.dto;
 
-public class User {
-    private String email;
-    private String password;
+public record User(String email, String password) {
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public User() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
