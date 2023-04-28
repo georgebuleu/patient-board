2f001814f7b2f34e8e8b2fc38384a0426a8b31bd
george <george-alexandru.buleu@student.tuiasi.ro>
2023-04-29 01:34:52 +0300

    fixed exception

--- patient-board/src/main/java/com/cegeka/academy/patienthub/service/AuthService.java
+++ patient-board/src/main/java/com/cegeka/academy/patienthub/service/AuthService.java
@@ -2,9 +2,9 @@ package com.cegeka.academy.patienthub.service;
 
 import com.cegeka.academy.patienthub.dto.User;
 
-import javax.naming.AuthenticationException;
+
 
 public interface AuthService {
 
-    void authenticate(User user) throws AuthenticationException;
+    void authenticate(User user);
 }
