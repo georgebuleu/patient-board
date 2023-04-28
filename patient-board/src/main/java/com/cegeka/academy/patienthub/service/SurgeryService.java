d50d871dcd613a8373831d9bad67467d08365155
george <george-alexandru.buleu@student.tuiasi.ro>
2023-04-29 02:22:53 +0300

    removed unnecessary annotation

--- patient-board/src/main/java/com/cegeka/academy/patienthub/service/SurgeryService.java
+++ patient-board/src/main/java/com/cegeka/academy/patienthub/service/SurgeryService.java
@@ -1,12 +1,6 @@
 package com.cegeka.academy.patienthub.service;
-
-import com.cegeka.academy.patienthub.model.Patient;
 import com.cegeka.academy.patienthub.model.Surgery;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
-@Service
 public interface SurgeryService {
     Surgery getByPatientId( Long patientId);
     void addSurgery(Surgery surgery, Long patientId);
