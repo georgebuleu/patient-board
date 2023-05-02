26c11cdd5df6e1b85c25d5ad0f811bc5d9071f52
biancamrd <e.bibu.e.b@gmail.com>
2023-05-02 21:13:55 +0300

    save and update diagnosis

--- patient-board/src/main/java/com/cegeka/academy/patienthub/service/DiagnosisService.java
+++ patient-board/src/main/java/com/cegeka/academy/patienthub/service/DiagnosisService.java
@@ -1,5 +1,7 @@
 package com.cegeka.academy.patienthub.service;
 
+import com.cegeka.academy.patienthub.DTO.DiagnosisDTO;
+import com.cegeka.academy.patienthub.DTO.TreatmentDTO;
 import com.cegeka.academy.patienthub.model.Diagnosis;
 
 import java.util.List;
@@ -8,5 +10,7 @@ public interface DiagnosisService {
 
     List<Diagnosis> findDiagnosisByPatientId(Long patientId);
 
+    void create(DiagnosisDTO diagnosisDTO);
 
+    Diagnosis update(Long id, DiagnosisDTO newDiagnosis);
 }
--- patient-board/src/main/java/com/cegeka/academy/patienthub/service/DiagnosisServiceImpl.java
+++ patient-board/src/main/java/com/cegeka/academy/patienthub/service/DiagnosisServiceImpl.java
@@ -1,7 +1,11 @@
 package com.cegeka.academy.patienthub.service;
 
+import com.cegeka.academy.patienthub.DTO.DiagnosisDTO;
 import com.cegeka.academy.patienthub.model.Diagnosis;
+import com.cegeka.academy.patienthub.model.Patient;
+import com.cegeka.academy.patienthub.model.Treatment;
 import com.cegeka.academy.patienthub.repository.DiagnosisRepository;
+import com.cegeka.academy.patienthub.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,13 +14,38 @@ import java.util.List;
 public class DiagnosisServiceImpl implements DiagnosisService{
 
     private final DiagnosisRepository diagnosisRepository;
+    private final PatientRepository patientRepository;
 
-    public DiagnosisServiceImpl(DiagnosisRepository diagnosisRepository) {
+
+    public DiagnosisServiceImpl(DiagnosisRepository diagnosisRepository, PatientRepository patientRepository) {
         this.diagnosisRepository = diagnosisRepository;
+        this.patientRepository = patientRepository;
     }
 
     @Override
     public List<Diagnosis> findDiagnosisByPatientId(Long patientId) {
         return diagnosisRepository.findDiagnosisByPatientId(patientId);
+    }
+
+    @Override
+    public void create(DiagnosisDTO diagnosisDTO) {
+        Diagnosis diagnosis = new Diagnosis();
+        diagnosis.setInitialDiagnosis(diagnosisDTO.getInitialDiagnosis());
+        diagnosis.setDiagnosisAfter72Hours(diagnosisDTO.getDiagnosisAfter72Hours());
+        diagnosis.setFinalDiagnosis(diagnosisDTO.getFinalDiagnosis());
+        Patient patient = patientRepository.findById(diagnosisDTO.getPatientId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Patient Id"));
+        diagnosis.setPatient(patient);
+        diagnosisRepository.save(diagnosis);
+    }
+
+    @Override
+    public Diagnosis update(Long id, DiagnosisDTO newDiagnosis) {
+        Diagnosis diagnosis = diagnosisRepository.findById(id).orElseThrow(() -> new RuntimeException("Diagnosis not found with id " + id));
+        diagnosis.setInitialDiagnosis(newDiagnosis.getInitialDiagnosis());
+        diagnosis.setDiagnosisAfter72Hours(newDiagnosis.getDiagnosisAfter72Hours());
+        diagnosis.setFinalDiagnosis(newDiagnosis.getFinalDiagnosis());
+        return diagnosisRepository.save(diagnosis);
+
     }
 }
